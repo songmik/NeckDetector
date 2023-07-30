2db0546deb29ebfdf72616c7a2de075af4b38fea
kwonsongmi <ksongmi0727@gmail.com>
2023-07-30 21:15:21 +0900

    #1 측정 화면 추가
    - 사진 찍는 부분, 측정 결과가 나오는 부분 화면 추가

--- app/src/main/java/com/mj/neckdetector/ui/activity/MeasureActivity.kt
+++ app/src/main/java/com/mj/neckdetector/ui/activity/MeasureActivity.kt
@@ -1,8 +1,16 @@
 package com.mj.neckdetector.ui.activity
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.mj.neckdetector.R
 import com.mj.neckdetector.databinding.ActivityMeasureBinding
+import com.mj.neckdetector.ui.fragment.TotalFragment
 
 class MeasureActivity : AppCompatActivity() {
 
@@ -13,6 +21,35 @@ class MeasureActivity : AppCompatActivity() {
         binding = ActivityMeasureBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        hideBottomBar()
 
+        binding.nextButton.setOnClickListener {
+            replaceFragment(TotalFragment())
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.measureContainer, fragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun hideBottomBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+
+            val controller = window.insetsController
+            if (controller != null) {
+                // navigationBars -> 하단바 제거
+                controller.hide(WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            window.decorView.systemUiVisibility =
+                (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
     }
 }
---
+++ app/src/main/java/com/mj/neckdetector/ui/fragment/TotalFragment.kt
@@ -0,0 +1,31 @@
+package com.mj.neckdetector.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mj.neckdetector.databinding.FragmentTotalBinding
+
+class TotalFragment : Fragment() {
+
+    private var _binding: FragmentTotalBinding ?= null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentTotalBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 단계별 텍스트를 나눠야 할 것 같음
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
